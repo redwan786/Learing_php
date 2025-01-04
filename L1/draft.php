772a73725e303aa4cc7dc45dc3f13467db31fec5
Md.Redwan Hossen <me.redwanhossen@gmail.com>
2025-01-04 23:49:29 +0600

    no warrning

--- L1/draft.php
+++ L1/draft.php
@@ -1,59 +1,52 @@
 <?php
-class Car{
+class Car {
     public $color;
     public $model;
 
-    public function __construct($color, $model){
+    public function __construct($color, $model) {
         $this->color = $color;
         $this->model = $model;
     }
-    public function message(){
+
+    public function message() {
         echo "Hello" . PHP_EOL;
         echo "\n";
         return "This is a " . $this->color . " " . $this->model . PHP_EOL;
     }
 }
 
-$myCar = new Car("red","volvo!");
+$myCar = new Car("red", "volvo!");
 var_dump($myCar);
 echo $myCar->message();
-
 
 $str = "Hello World from Bangladesh";
 echo strlen($str) . PHP_EOL;
 echo str_word_count($str) . PHP_EOL;
 echo strpos($str, "from") . PHP_EOL;
 
-
-
-function modifyArray($arr){
-    foreach ($arr as $index => $value){
+function modifyArray($arr) {
+    foreach ($arr as $index => $value) {
         $arr[$index] = $value * 2;
     }
     return $arr;
 }
-$data = [1,2,3,4];
+
+$data = [1, 2, 3, 4];
 $newData = modifyArray($data);
 
 print_r($data);
 print_r($newData);
 
-
-
-function modifyArray2(&$arr2){
-    foreach ($arr as $index => $value){
+function modifyArray2(&$arr2) { // Correct parameter name
+    foreach ($arr2 as $index => $value) { // Use $arr2 here
         $arr2[$index] = $value * 2;
     }
     return $arr2;
 }
-$data2 = [1,2,3,4];
+
+$data2 = [1, 2, 3, 4];
 $newData2 = modifyArray2($data2);
 
 print_r($data2);
 print_r($newData2);
-
-
-
-
-
 ?>
